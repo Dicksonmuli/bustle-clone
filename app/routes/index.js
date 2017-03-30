8eaa82e391c2417bb5689c241910126f4deb3c97
dicksonmuli <dicksonmully6@gmail.com>
2017-03-30 12:56:08 +0300

    saving new post- setting actions for saving new post

--- app/routes/index.js
+++ app/routes/index.js
@@ -4,11 +4,6 @@ export default Ember.Route.extend({
     model() {
     return this.store.findAll('post');
   },
-  actions: {
-    savePost3(params){
-      var newPost = this.store.createRecord('post', params);
-      newPost.save();
-      this.transitionTo('index');
-    }
+
   }
 });
